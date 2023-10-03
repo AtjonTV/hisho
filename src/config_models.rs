4a098033fce860c391ba7055fb87f82bf6b99cf5
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-03 09:50:57 +0200

    Make values optional

--- src/config_models.rs
+++ src/config_models.rs
@@ -4,29 +4,38 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Service {
     pub name: String,
+    #[serde(default)]
     pub environments: Vec<Environment>,
+    #[serde(default)]
     pub containers: Vec<Container>,
+    #[serde(default)]
     pub commands: Vec<Command>,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Environment {
     pub name: String,
+    #[serde(default)]
     pub inherits: Vec<String>,
+    #[serde(default)]
     pub values: HashMap<String, String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Container {
     pub name: String,
+    #[serde(default)]
     pub required: bool,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Command {
     pub name: String,
+    #[serde(default)]
     pub environment: String,
     pub shell: Vec<String>,
+    #[serde(default)]
     pub args: HashMap<String, String>,
+    #[serde(default)]
     pub capture_all: bool,
 }
