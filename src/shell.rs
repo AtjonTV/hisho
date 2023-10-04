f2cda3a68c739ff93ac22f83741d7d8ec953cf3a
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-04 10:31:59 +0200

    Change from reading the output into a variable to let it stream to stdout

--- src/shell.rs
+++ src/shell.rs
@@ -1,26 +1,18 @@
 use std::collections::HashMap;
 use std::io;
-use std::process::Output;
+use std::process::ExitStatus;
 use crate::config_models::Environment;
 
-pub fn exec(command: &str, env: &Option<Environment>) -> io::Result<Output> {
+pub fn exec(command: &str, env: &Option<Environment>) -> io::Result<ExitStatus> {
     // execute the command in /bin/sh
     let mut proc_command = std::process::Command::new("sh");
     proc_command.args(["-c", command.clone()]);
     proc_command.envs(if let Some(e) = &env { e.values.clone() } else { HashMap::new() });
 
     // Check if the command succeeded
-    let proc_result = proc_command.output();
+    let proc_result = proc_command.status();
     if let Ok(output) = &proc_result {
-        if let Ok(text) = String::from_utf8(output.stdout.clone()) {
-            println!("{}", text);
-        }
-        if let Ok(text) = String::from_utf8(output.stderr.clone()) {
-            if !text.is_empty() {
-                println!("{}", text);
-            }
-        }
-        println!("Command '{}' executed. ({})", command, output.status);
+        println!("Command '{}' executed. ({})", command, output);
     } else {
         println!("Could not execute command.");
     }
