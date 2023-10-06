2dd80b2942d70316e4b692204ec4acd56b168443
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-06 12:50:58 +0200

    Add .space.kts for automated builds

---
+++ .space.kts
@@ -0,0 +1,21 @@
+job("Build") {
+	startOn {
+		anyTagMatching {
+			+"v*"
+		}
+	}
+    container(displayName = "Run script", image = "rustlang/rust:nightly") {
+        shellScript {
+            content = """
+                set -e
+                rustup target add x86_64-unknown-linux-gnu
+                # Build the Rust project
+                cargo build --verbose --release --target x86_64-unknown-linux-musl
+            """
+        }
+		fileArtifacts {
+			remotePath = "service_helper-musl.bin"
+			localPath = "target/x86_64-unknown-linux-musl/release/service_helper"
+		}
+    }
+}
