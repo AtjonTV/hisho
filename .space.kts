afd335b142bb911e5ae64fe2167cfc14fb0612c0
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-06 15:40:06 +0200

    Try to cache the rustlib in .space.kts

--- .space.kts
+++ .space.kts
@@ -23,5 +23,9 @@ job("Build") {
 			storeKey = "cargo-{{ hashFiles('Cargo.lock') }}"
 			localPath = "target"
 		}
+		cache {
+			storeKey = "rustlib"
+			localPath = "/usr/local/rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-musl"
+		}
     }
 }
