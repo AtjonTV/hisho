748c8941314a7939df799f1c99bd534a4cb7a366
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-14 12:38:43 +0200

    Add missing license header and fix error in changelog

--- src/hisho/git.rs
+++ src/hisho/git.rs
@@ -1,3 +1,8 @@
+// Copyright 2023 Thomas Obernosterer (https://atjon.tv).
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use gix::date::time::format::ISO8601_STRICT;
 use std::collections::HashMap;
 use std::path::Path;
