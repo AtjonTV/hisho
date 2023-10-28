8942838d4e6ae30e2706e4541cf9b6fd3acc41e5
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-28 11:52:40 +0200

    Update Copyright header in all source files

--- hisho_core/src/lib.rs
+++ hisho_core/src/lib.rs
@@ -1,4 +1,7 @@
+// This file 'lib.rs' is part of the 'hisho' project.
+//
 // Copyright 2023 Thomas Obernosterer (https://atjon.tv).
+//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
