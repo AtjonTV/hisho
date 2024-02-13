504a6d83ba2f28ae8c8d4af67c2833ce72329ba3
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2024-02-13 15:05:34 +0100

    Misc: Update Copyright notice

--- hisho_cli2/src/main.rs
+++ hisho_cli2/src/main.rs
@@ -1,6 +1,6 @@
 // This file 'main.rs' is part of the 'hisho' project.
 //
-// Copyright 2023 Thomas Obernosterer (https://atjon.tv).
+// Copyright 2023-2024 Thomas Obernosterer (https://atjon.tv).
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
